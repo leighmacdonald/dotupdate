74ae60daede45180a75efafbcf988b8a61cec8c9
Leigh MacDonald <leigh@cudd.li>
2012-11-04 15:32:01 -0700

    Test current passing

--- dotupdate/__init__.py
+++ dotupdate/__init__.py
@@ -1,8 +1,68 @@
 from glob import glob
-from os.path import isfile, isdir, islink
+from os.path import isfile, isdir, islink, abspath, lexists, exists, join, expanduser
+from os import symlink
 
-def install(source_path="./", source_filter="*", dest_path="~/"):
+
+class InvalidConfiguration(Exception):
+    """Used when a user configures bad paths"""
+
+
+class LinkExists(OSError):
+    pass
+
+
+class FileExists(OSError):
+    pass
+
+
+def install(source_path=".", source_filter="*", dest_path="~/"):
+    """ Install a set of files from the specified source path into the dest_path.
+
+    An example of the transformation that will take place:
+
+    source path : ./my_dot_files
+                  ./my_dot_files/bashrc
+                  ./my_dot_files/vim
+    dest_path   : ~/
+                  ~/.bashrc
+                  ~/.vim
+
+
+    :param source_path:
+    :type source_path:
+    :param source_filter:
+    :type source_filter:
+    :param dest_path:
+    :type dest_path:
+    :return:
+    :rtype:
+    """
+    full_source_path = abspath(source_path)
+    full_dest_path = abspath(expanduser(dest_path))
     filter_txt = "{0}/{1}".format(source_path, source_filter)
     potential_links = glob(filter_txt)
+    if not potential_links:
+        error_msg = "No potential candidates for linking found at {0}".format(source_path)
+        raise InvalidConfiguration(error_msg)
+
     for link in potential_links:
-        print(link)
+        if link[0:2] == "./":
+            link = link[2:]
+        link = link[len(source_path) - 1:]
+        dest_link = join(full_dest_path, '.' + link)
+        source_link = join(full_source_path, link)
+        try:
+            if exists(dest_link):
+                raise FileExists("File exists already {0}".format(dest_link))
+            if lexists(dest_link):
+                raise LinkExists("Link exists {0}".format(dest_link))
+        except FileExists:
+            pass
+        except LinkExists:
+            pass
+
+        try:
+            symlink(source_link, dest_link)
+        except OSError as err:
+            print(err)
+
--- tests/test_install.py
+++ tests/test_install.py
@@ -1,6 +1,6 @@
 import unittest
 from os import mkdir
-from os.path import exists, join
+from os.path import exists, join, islink, lexists
 from shutil import rmtree
 from dotupdate import install
 
@@ -12,10 +12,13 @@ class TestInstall(unittest.TestCase):
         if exists(self.source_root):
             rmtree(self.source_root)
         mkdir(self.source_root)
+        if exists(self.dest_root):
+            rmtree(self.dest_root)
+        mkdir(self.dest_root)
         for d in ['dir1', 'dir2', 'dir2/subdir1']:
             mkdir(join(self.source_root, d))
-        open(join(self.source_root, 'file.test'), 'w').write("x" * 100)
-        open(join(self.source_root, 'dir1', 'file2.test'), 'w').write("x" * 100)
+        open(join(self.source_root, 'file.test'), 'w').write("hi")
+        open(join(self.source_root, 'dir1', 'file2.test'), 'w').write("hi")
 
     def tearDown(self):
         rmtree(self.source_root)
@@ -23,7 +26,10 @@ class TestInstall(unittest.TestCase):
 
     def test_install(self):
         install(self.source_root, dest_path=self.dest_root)
-        self.assertEqual(True, False)
+        for path in [join(self.dest_root, '.file.test'),
+                     join(self.dest_root, '.dir1', 'file2.test'),
+                     join(self.dest_root, '.dir2', 'subdir1')]:
+            self.assertTrue(lexists(path))
 
 if __name__ == '__main__':
     unittest.main()
